75577affa379eb877190acfc6f5f426148bcd80b
arbelteitel <arbeltaitel@gmail.com>
2025-09-28 14:11:57 +0300

    hehe forgot k8s

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -5,11 +5,27 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+# Configure Kubernetes provider to connect to EKS
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+  }
 }
 
 module "eks_cluster" {
